318670b41d1d1e69773b486ea35fa17e9572366c
Abdulhekim abdullah <abdalhkemkanbr650@gmail.com>
2023-04-12 00:43:23 +0300

    push

--- layout/pageWithLayouts.ts
+++ layout/pageWithLayouts.ts
@@ -1,13 +1,7 @@
 import { NextPage } from "next";
 import type { ReactElement } from "react";
-import MainLayout from "./admin/index";
-import AdminLayout from "./stores/index";
 
-export type PageWithMainLayoutType = NextPage & { layout: typeof MainLayout }
-export type PageWithAdminLayoutType = NextPage & { layout: typeof AdminLayout }
-export type PageWithLayoutType =
-    | PageWithMainLayoutType
-    | PageWithAdminLayoutType
+export type PageWithLayoutType = any
 
 export type LayoutProps = ({ children }: { children: ReactElement }) => ReactElement
 export default PageWithLayoutType;
